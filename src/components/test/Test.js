1bc64835e85d46fabf7de3798da74a8df5cb845b
melowsoft <melowsworld@yahoo.co.uk>
2018-09-21 07:44:05 +0100

    version 1.0

---
+++ src/components/test/Test.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class Test extends Component {
+  state = {
+    title: "",
+    body: ""
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          title: data.title,
+          body: data.body
+        })
+      );
+  }
+
+  render() {
+    const { title } = this.state;
+    return <h1>{title}</h1>;
+  }
+}
+
+export default Test;
